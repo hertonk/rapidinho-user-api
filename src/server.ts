dd7c2ce29c4522eb364ec21f2c4f9e10398f7913
Herton Vilarim <hertonk@gmail.com>
2022-02-13 12:32:14 -0300

    Fix

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import "./database";
 
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json({limit:'50mb'})); 
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
